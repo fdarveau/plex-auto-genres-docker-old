33eb79560c264639698df792a57a098f9f39bfec
François Darveau <franklebest07@gmail.com>
2020-12-30 13:25:40 -0500

    fix path to plex-auto-genres.py so it works during the schedules cron job

--- generate-collections.sh
+++ generate-collections.sh
@@ -6,7 +6,7 @@ while IFS= read -r line; do
         echo "Processing $line";
         library=$(echo $line | cut -d ";" -f 1);
         type=$(echo $line | cut -d ";" -f 2);
-        python3 plex-auto-genres.py --library "$library" --type "$type";
+        python3 /plex-auto-genres.py --library "$library" --type "$type";
         echo;
     fi
 done < /config/libraries.txt
